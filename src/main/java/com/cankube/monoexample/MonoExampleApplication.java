33dad7d68adfb680399648c22fdf245b4343d302
Vu Vo <vvo@tempo.io>
2020-02-16 12:52:52 -0500

    first commit

---
+++ src/main/java/com/cankube/monoexample/MonoExampleApplication.java
@@ -0,0 +1,13 @@
+package com.cankube.monoexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonoExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonoExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cankube/monoexample/MonoExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cankube.monoexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonoExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
